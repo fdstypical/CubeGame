958456ef09c651c14e219ce4e193afc054f6163d
Andrey Shulmin <networkdaemonyung@gmail.com>
2020-11-08 00:37:37 +0300

    init simple game

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(h * speed, rb.velocity.y, v * speed);
+
+        rb.AddForce(movement);
+    }
+}
