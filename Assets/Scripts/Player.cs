0c223be2388093c2c33ae1e3318e9b41ae70767d
Andrey Shulmin <networkdaemonyung@gmail.com>
2020-11-10 22:30:20 +0300

    add cube rotation with mouse movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float mouseSens = 5;
     public float speed;
+    float mouseX;
+
 
     Rigidbody rb;
 
@@ -14,10 +17,14 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        mouseX = Input.GetAxis("Mouse X") * mouseSens;
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+
+        Quaternion rotationY =Quaternion.AngleAxis(mouseX, Vector3.up); 
         Vector3 movement = new Vector3(h * speed * Time.deltaTime, 0, v * speed * Time.deltaTime);
 
         transform.Translate(movement);
+        transform.rotation *= rotationY;
     }
 }
